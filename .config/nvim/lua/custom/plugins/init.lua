931246a1aebf53f390a2050dfea7da97335ac18b
Amir Sahrani <amirsahrani1@gmail.com>
2024-12-28 19:32:29 +0100

    tex support

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -62,4 +62,14 @@ return {
     -- Optional dependencies
     dependencies = { { 'nvim-tree/nvim-web-devicons', opts = {} } },
   },
+  {
+    'lervag/vimtex',
+    lazy = false, -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    init = function()
+      -- VimTeX configuration goes here, e.g.
+      vim.g.vimtex_view_method = 'zathura'
+      vim.g.vimtex_compiler_method = 'pdflatex'
+    end,
+  },
 }
--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -5,3 +5,4 @@ vim.keymap.set('n', '<Tab>', '<CMD>BufferNext<CR>', { desc = 'Open Parent Direct
 vim.keymap.set('n', '<S-Tab>', '<CMD>BufferPrevious<CR>', { desc = 'Open Parent Directory' })
 vim.keymap.set('v', '<leader>/', '<Plug>(comment_toggle_blockwise_visual)', { desc = 'Block Comment' })
 vim.keymap.set('n', '<leader>/', '<Plug>(comment_toggle_linewise_current)', { desc = 'Line Comment' })
+vim.keymap.set('n', '<leader>ww', '<CMD>set linebreak<CR>', { desc = 'Line Comment' })
